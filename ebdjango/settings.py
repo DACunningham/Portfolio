a48ea77383fc2af2ca2522473e65d7aa27d53e3f
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-09-16 12:17:04 +0100

    Add MySQL db connection

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = "ebdjango.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "portfolio",
+        "USER": "dexter",
+        "PASSWORD": "Public-django-db",
+        "HOST": "portfolio.cluster-ct6vqujv0vuv.eu-west-2.rds.amazonaws.com",
+        "PORT": "3306",
     }
 }
 
