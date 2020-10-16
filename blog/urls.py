daaa0bc4ae993c749d35f4165337a3c489aa0153
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-10-16 17:16:05 +0100

    Implement generic views

--- blog/urls.py
+++ blog/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:pk>/", views.detail, name="detail"),
+    # path("", views.index, name="index"),
+    path("", views.ArticleList.as_view(), name="index"),
+    path("<int:pk>/", views.ArticleDetail.as_view(), name="detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Article
 
-# Create your views here.
-def index(request, *args, **kwargs):
-    qs = Article.objects.all()
-    return render(request, "blog/index.html", context={"articles": qs})
+
+class ArticleList(ListView):
+    model = Article
+    template_name = "blog/index.html"
 
 
-def detail(request, pk, *args, **kwargs):
-    article = get_object_or_404(Article, pk=pk)
-    return render(request, "blog/detail.html", context={"article": article})
+class ArticleDetail(DetailView):
+    model = Article
+    template_name = "blog/detail.html"
+    context_object_name = "article"
